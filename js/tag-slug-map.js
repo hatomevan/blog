81de6f17ba76b7657d20ce6f7d878171a1080cd5
hatomevan <91764112+hatomevan@users.noreply.github.com>
2025-05-26 23:43:16 +0900

    Add files via upload

---
+++ js/tag-slug-map.js
@@ -0,0 +1,11 @@
+// public/js/tag-slug-map.js
+window.tagSlugMap = {
+  "limbuscompany": "limbuscompany",
+  "読書": "reading",
+  "食事": "eat",
+  "夢": "dream",
+  "マーダーミステリー": "murdermystery",
+  "実況": "live",
+  "ゲーム": "game",
+  "英語": "english",
+};
